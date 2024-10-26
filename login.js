2dc77e00c246892320b74f435e7d2b7ecb18299c
Bruno Viotto <brunoviottosantos@gmail.com>
2024-10-26 20:38:07 -0300

    refactor: Extraindo css comum entre páginas em um único arquivo styles.css global

--- login.js
+++ login.js
@@ -1,23 +1,23 @@
-document.getElementById('login').addEventListener('submit', function(event) {
-  event.preventDefault();
+document.getElementById('login').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-  // Obtendo os valores do formulário
-  const email = document.getElementById('email').value;
-  const senha = document.getElementById('senha').value;
+    // Obtendo os valores do formulário
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
 
-  /* Recuperando a lista de usuários armazenada no localStorage
-  Recupera os dados de usuários armazenados no localStorage. Se não houver nenhum, retorna um array vazio.*/
-  const users = JSON.parse(localStorage.getItem('users')) || [];
+    /* Recuperando a lista de usuários armazenada no localStorage
+    Recupera os dados de usuários armazenados no localStorage. Se não houver nenhum, retorna um array vazio.*/
+    const users = JSON.parse(localStorage.getItem('users')) || [];
 
-  /* Verificando se existe um usuário com o email e senha fornecidos
-  O método find() percorre o array de usuários e verifica se existe algum usuário com o email e a senha fornecidos.*/
-  const user = users.find(user => user.email === email && user.password === senha);
+    /* Verificando se existe um usuário com o email e senha fornecidos
+    O método find() percorre o array de usuários e verifica se existe algum usuário com o email e a senha fornecidos.*/
+    const user = users.find(user => user.email === email && user.password === senha);
 
-  if (user) {
-    alert('Login bem-sucedido!');
-    // Redireciona para a página de boas-vindas
-    window.location.href = 'tela_jogos.html';
-  } else {
-    alert('Email ou senha incorretos.');
-  }
+    if (user) {
+        alert('Login bem-sucedido!');
+        // Redireciona para a página de boas-vindas
+        window.location.href = 'tela_jogos.html';
+    } else {
+        alert('Email ou senha incorretos.');
+    }
 });
